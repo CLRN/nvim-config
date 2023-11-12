b1a344be0465cf9b86c39b6f52d0793612dc30e9
skarnaukhov <skarnaukhov@bloomberg.net>
2023-11-12 13:32:17 +0000

    debug keymaps

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -12,7 +12,15 @@ local M = {}
 M.dap = {
   plugin = true,
   n = {
-    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"},
+    ["<leader>du"] = {"<cmd> lua require('dapui').toggle() <CR>"},
+    ["<leader>df"] = {"<cmd> lua require('dapui').float_element('scopes') <CR>"},
+    ["<leader>de"] = {"<cmd> lua require('dapui').eval() <CR>"},
+    ["<F6>"] = {"<cmd> DapToggleBreakpoint <CR>"},
+    ["<F8>"] = {"<cmd> DapStepOver <CR>"},
+    ["<F7>"] = {"<cmd> DapStepInto <CR>"},
+    ["<F9>"] = {"<cmd> DapContinue <CR>"},
+    ["<F4>"] = {"<cmd> DapTerminate <CR>"},
   }
 }
 

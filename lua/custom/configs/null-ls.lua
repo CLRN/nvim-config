475ae5dc8f4b49c7a48500dcb5c45d067d687164
Sergey Karnaukhov <skarnaukhov@bloomberg.net>
2023-12-12 17:31:19 +0000

    adjust formatters

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -6,11 +6,11 @@ local opts = {
   sources = {
     null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
-    null_ls.builtins.diagnostics.curlylint,
-    null_ls.builtins.formatting.prettier.with { filetypes = { "html", "markdown", "css", "htmldjango" } }, -- so prettier works only on these filetypes
+    null_ls.builtins.formatting.prettier.with { filetypes = { "markdown", "css" } },
     null_ls.builtins.formatting.clang_format,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.beautysh,
+    null_ls.builtins.formatting.djhtml.with { filetypes = { "html", "htmldjango" } },
 
     -- python
     null_ls.builtins.formatting.black.with({

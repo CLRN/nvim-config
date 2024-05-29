496612e253159b08d71fadccaeb2c27a4cf5b8d4
Sergey Karnaukhov <skarnaukhov@bloomberg.net>
2024-05-29 14:57:38 -0400

    try term edit plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -746,6 +746,15 @@ local plugins = {
       "tpope/vim-rhubarb",
     },
   },
+  {
+    "chomosuke/term-edit.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("term-edit").setup {
+        prompt_end = '$',
+      }
+    end
+  },
 
   -- {
   --   "glacambre/firenvim",

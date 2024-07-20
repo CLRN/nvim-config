4dd9c2338d2faef0bf7a8bfa7324e1d0388a3195
skarnaukhov <skarnaukhov@bloomberg.net>
2024-07-20 10:11:38 +0100

    new plugin for marks

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -751,9 +751,9 @@ local plugins = {
     event = "VeryLazy",
     config = function()
       require("term-edit").setup {
-        prompt_end = ' ',
+        prompt_end = " ",
       }
-    end
+    end,
   },
 
   {
@@ -772,6 +772,42 @@ local plugins = {
     dependencies = {
       "nvim-telescope/telescope.nvim",
     },
+  },
+
+  {
+    "tomasky/bookmarks.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("bookmarks").setup {
+        -- sign_priority = 8, --set bookmark sign priority to cover other sign
+        save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
+        keywords = {
+          ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+          ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+          ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+          ["@n"] = "󰈚 ", -- mark annotation startswith @n ,signs this icon as `Note`
+        },
+        on_attach = function(bufnr)
+          local bm = require "bookmarks"
+          local map = vim.keymap.set
+          map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
+          map("n", "ma", bm.bookmark_ann) -- add or edit mark annotation at current line
+          map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
+          map("n", "m'", bm.bookmark_next) -- jump to next mark in local buffer
+          map("n", "mj", bm.bookmark_prev) -- jump to previous mark in local buffer
+          map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
+          map("n", "mx", bm.bookmark_clear_all) -- removes all bookmarks
+        end,
+      }
+      require("telescope").load_extension "bookmarks"
+
+      vim.api.nvim_set_keymap(
+        "n",
+        "ms",
+        ":lua require('telescope').extensions.bookmarks.list()<CR>",
+        { noremap = true, silent = true, desc = "Search bookmarks" }
+      )
+    end,
   },
   -- {
   --   "glacambre/firenvim",
